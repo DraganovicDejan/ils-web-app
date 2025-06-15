c673388a55a7869188441a1829835bccd490682f
start.vaadin.com <no-reply@vaadin.com>
2025-06-15 06:11:07 +0000

    Generated project

---
+++ src/main/java/at/spengergasse/ils/base/ui/view/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable or cross-cutting view-related classes.
+ */
+@NullMarked
+package at.spengergasse.ils.base.ui.view;
+
+import org.jspecify.annotations.NullMarked;
